1228fc253aaa4fa4310c9b9da1ef7e8e7a93f1a2
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-07-11 17:17:07 +0200

    feature - map screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
 import { createStackNavigator } from '@react-navigation/stack';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const MapScreen = () => {
-
+  const navigation = useNavigation();
   const Stack = createStackNavigator();
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
@@ -33,7 +41,7 @@ const MapScreen = () => {
           />
         </Stack.Navigator>
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
 
